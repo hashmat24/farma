31d3171abc22b103e99cae4f50208b986b4338af
Hashmat Khan <hashmatkhan70042@gmail.com>
2026-02-25 18:13:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3869996886-48dd1",
+  "appId": "1:435505303723:web:2180b0195520cc8c3727ee",
+  "apiKey": "AIzaSyCzB9lXcgooQtqh9vb_o6fPRZjxX4ppXRk",
+  "authDomain": "studio-3869996886-48dd1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "435505303723"
+};
