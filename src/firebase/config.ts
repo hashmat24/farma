769a6789ca69223829882f5449bfb6f3e66e11a4
Hashmat Khan <hashmatkhan70042@gmail.com>
2026-03-01 04:40:56 +0000

    AIzaSyCtxoVdSeX0ag2R5PyiNS9JI_dCDhiEPoY
    take this and chage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3869996886-48dd1",
   "appId": "1:435505303723:web:2180b0195520cc8c3727ee",
-  "apiKey": "AIzaSyCzB9lXcgooQtqh9vb_o6fPRZjxX4ppXRk",
+  "apiKey": "AIzaSyCtxoVdSeX0ag2R5PyiNS9JI_dCDhiEPoY",
   "authDomain": "studio-3869996886-48dd1.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "435505303723"
